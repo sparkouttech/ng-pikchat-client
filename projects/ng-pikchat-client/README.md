549ab8c3be0206c3bf563736e8e5476e2e7a0fd0
Sivabharathy <gotocva@gmail.com>
2021-12-06 13:17:45 +0530

    doc library updated

--- projects/ng-pikchat-client/README.md
+++ projects/ng-pikchat-client/README.md
@@ -1,5 +1,61 @@
 # NgPikchatClient
 
+## Dependencies
+
+Latest version available for each version of Angular
+
+| ng-pikchat-client | Angular     |
+| ----------------- | ----------- |
+| current           | >= 10.x     |
+
+## Install
+
+```bash
+npm install ng-pikchat-client --save
+```
+
+**step 1:** add NgPikchatClientModule to app NgModule
+
+```typescript
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NgPikchatClientModule } from "ng-pikchat-client";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgPikchatClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+```
+
+## Use
+
+```typescript
+import { NgPikchatClientService } from "ng-pikchat-client";
+
+@Component({...})
+export class YourComponent {
+  constructor(private pikchatClient: NgPikchatClientService) {}
+
+  testConnection() {
+    console.log(this.pikchatClient);
+  }
+}
+
+```
+
 This library was developed by [https://www.pikchat.co](https://www.pikchat.co) 
 
 ## License
--- projects/ng-pikchat-client/src/lib/ng-pikchat-client.service.ts
+++ projects/ng-pikchat-client/src/lib/ng-pikchat-client.service.ts
@@ -3,12 +3,23 @@ import { Observable } from 'rxjs';
 
 import * as socketIO from 'socket.io-client';
 
+const events = {
+  DISCONNECT: 'disconnect',
+  GET_MESSAGES: 'GET_MESSAGES',
+  OFFLINE_MESSAGES: 'OFFLINE_MESSAGES',
+  ONLINE_USERS: 'ONLINE_USERS',
+  SINGLE_CHAT_MESSAGE : 'SINGLE_CHAT_MESSAGE',
+  SINGLE_CHAT_MESSAGE_DELIVERED : 'SINGLE_CHAT_MESSAGE_DELIVERED',
+  SINGLE_CHAT_MESSAGE_READ : 'SINGLE_CHAT_MESSAGE_READ'
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class NgPikchatClientService {
 
   socket: any = null;
+  public EVENTS : object = events;
   private uri: string = 'http://localhost:3000?userId=5&accessToken=25&sessionId=345&apiKey=67gh6';
 
   constructor() { }
@@ -21,6 +32,107 @@ export class NgPikchatClientService {
     this.uri = url;
   }
 
+  /**
+   * 
+   * @returns 
+   */
+  public getMessagesListener() {
+    return this.listen(events.GET_MESSAGES);
+  };
+
+  /**
+   * 
+   * @param data 
+   * @returns 
+   */
+  public emitGetMessages(data: any) {
+    return this.emit(events.GET_MESSAGES, data);
+  };
+
+  /**
+   * 
+   */
+  public oflineMessagesListener() {
+    return this.listen(events.OFFLINE_MESSAGES);
+  };
+
+  /**
+   * 
+   * @param data 
+   * @returns 
+   */
+  public emitOflineMessages(data: any) {
+    return this.emit(events.OFFLINE_MESSAGES, data);
+  };
+
+  /**
+   * 
+   * @returns 
+   */
+  public onlineUsersListener() {
+    return this.listen(events.ONLINE_USERS);
+  };
+
+  /**
+   * 
+   * @param data 
+   * @returns 
+   */
+  public emitOnlineUsers(data: any) {
+    return this.emit(events.ONLINE_USERS, data);
+  };
+
+  /**
+   * 
+   * @returns 
+   */
+  public singleChatMessageListener() {
+    return this.listen(events.SINGLE_CHAT_MESSAGE);
+  };
+
+  /**
+   * 
+   * @param data 
+   * @returns 
+   */
+  public emitSingleChatMessages(data: any) {
+    return this.emit(events.SINGLE_CHAT_MESSAGE, data);
+  };
+
+  /**
+   * 
+   * @returns 
+   */
+  public singleChatMessageDeliveredListener() {
+    return this.listen(events.SINGLE_CHAT_MESSAGE_DELIVERED);
+  };
+
+  /**
+   * 
+   * @param data 
+   * @returns 
+   */
+  public emitSingleChatMessageDelivered(data: any) {
+    return this.emit(events.SINGLE_CHAT_MESSAGE_DELIVERED, data);
+  };
+
+  /**
+   * 
+   * @returns 
+   */
+  public singleChatMessageReadListener() {
+    return this.listen(events.SINGLE_CHAT_MESSAGE_READ);
+  };
+
+  /**
+   * 
+   * @param data 
+   * @returns 
+   */
+  public emitSingleChatMessageRead(data: any) {
+    return this.emit(events.SINGLE_CHAT_MESSAGE_READ, data);
+  };
+  
   /**
    * 
    * @returns 
